d33e9528a71bdee50c61d2357cebf97f6be681ae
kogi <briankogi18@gmail.com>
2023-03-07 15:01:11 +0000

    store job application in database

--- database.py
+++ database.py
@@ -28,15 +28,24 @@ def load_job_from_db(id):
     if len(rows) == 0:
       return None
     else:
-      # print(column_names)
-      # print(rows)
       jobs = None
       column_names = result.keys()
       for row in rows:
         jobs=dict(zip(column_names, row))
-        # jobs.append(dict(zip(column_names, row)))
       return(jobs)
       
 
-load_job_from_db(3)
-# print(load_jobs_from_db())
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+
+    conn.execute(query, 
+                 {'job_id':job_id, 
+                 'full_name':data['full_name'],
+                 'email':data['email'],
+                 'linkedin_url':data['linkedin_url'],
+                 'education':data['education'],
+                 'work_experience':data['work_experience'],
+                 'resume_url':data['resume_url']})
+
+
